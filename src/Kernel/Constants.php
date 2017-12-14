ccab5b4454545ab134b46a80b41dd32159ebab2e
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2017-12-14 05:05:57 +0500

    1.1.2

--- src/Kernel/Constants.php
+++ src/Kernel/Constants.php
@@ -10,9 +10,9 @@ namespace Comely\Framework\Kernel;
 interface Constants
 {
     /** string Version (Major.Minor.Patch) */
-    const VERSION   =   "1.1.1";
+    const VERSION   =   "1.1.2";
     /** int Version (Major * 10000 + Minor * 100 + Patch) */
-    const VERSION_ID    =   10101;
+    const VERSION_ID    =   10102;
 
     const DS    =   DIRECTORY_SEPARATOR;
     const EOL   =   PHP_EOL;
