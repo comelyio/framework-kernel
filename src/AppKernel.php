f71b5bf643877c1645a69be43233f66487563be5
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-16 07:36:12 +0500

    Copyright

---
+++ src/AppKernel.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of Comely Framework Kernel package.
+ * https://github.com/comelyio/framework-kernel
+ *
+ * Copyright (c) 2016-2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/framework-kernel/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\Framework;
+
+/**
+ * Class AppKernel
+ * @package Comely\Framework
+ */
+class AppKernel
+{
+    private $config;
+    private $dateTime;
+    private $directories;
+    private $errorHandler;
+    private $memory;
+    private $services;
+
+    public function __construct(array $options, string $env)
+    {
+
+    }
+
+
+}
---
+++ src/AppKernel/DateTime.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Comely Framework Kernel package.
+ * https://github.com/comelyio/framework-kernel
+ *
+ * Copyright (c) 2016-2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/framework-kernel/blob/master/LICENSE
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: mfasp
+ * Date: 2/16/2018
+ * Time: 7:34 AM
+ */
