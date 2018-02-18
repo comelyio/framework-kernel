11b778dc94e9822598e64c600f79f705230f7f88
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2018-02-19 04:44:18 +0500

    Services

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -17,9 +17,16 @@ namespace Comely\Framework;
 use Comely\Framework\AppKernel\Config;
 use Comely\Framework\AppKernel\DateTime;
 use Comely\Framework\AppKernel\Directories;
+use Comely\Framework\AppKernel\Services;
 use Comely\Framework\Exception\BootstrapException;
+use Comely\Framework\Exception\ConfigException;
+use Comely\IO\Cache\Cache;
+use Comely\IO\Cipher\Cipher;
 use Comely\IO\FileSystem\Disk\Directory;
 use Comely\IO\FileSystem\Exception\DiskException;
+use Comely\IO\Session\ComelySession;
+use Comely\IO\Translator\Translator;
+use Comely\Knit\Knit;
 
 /**
  * Class AppKernel
@@ -27,24 +34,25 @@ use Comely\IO\FileSystem\Exception\DiskException;
  */
 class AppKernel
 {
-    protected const DIR_CONFIG = "app/config";
-    protected const DIR_CACHE = "tmp/cache";
-    protected const DIR_COMPILER = "tmp/compiler";
-    protected const DIR_LANGS = "app/langs";
-    protected const DIR_LOGS = "tmp/logs";
-
-    public const MODE_DEV = 1101;
-    public const MODE_PROD = 1102;
+    protected const DIR_CONFIG = "config";
+    protected const DIR_CACHE = "cache";
+    protected const DIR_COMPILER = "compiler";
+    protected const DIR_LANGS = "translator";
+    protected const DIR_LOGS = "logs";
+    protected const DIR_SESSIONS = "sessions";
 
     /** @var Config */
     private $config;
     /** @var DateTime */
     private $dateTime;
+    /** @var bool */
+    private $dev;
     /** @var Directories */
     private $directories;
 
     private $errorHandler;
     private $memory;
+    /** @var Services */
     private $services;
 
     /**
@@ -54,11 +62,13 @@ class AppKernel
      */
     public function __construct(array $options, string $env)
     {
-        // Server type
-        $serverType = $options["server"] ?? null;
-        if (!$serverType || !in_array($serverType, ["web", "cli"])) {
-            throw new BootstrapException('Invalid or no value for "server" option');
+        // Development mode
+        $dev = $options["dev"] ?? $dev["development"] ?? null;
+        if (!is_bool($dev)) {
+            throw new BootstrapException('Option "dev" must be of type boolean');
         }
+
+        $this->dev = $dev;
 
         // Root directory
         $rootPath = $options["rootPath"] ?? $options["root_path"] ?? null;
@@ -79,10 +89,93 @@ class AppKernel
             throw new BootstrapException('Invalid value for "loadCachedConfig" option');
         }
 
-        $this->config = new Config($this, $env);
+        $this->configure($loadCachedConfig, $env); // Bootstrap configuration
 
+        // Date Time
+        $this->dateTime = (new DateTime())
+            ->setTimezone($this->config->timeZone());
 
-        $this->dateTime = new DateTime();
+        // Services
+        $this->services = new Services($this);
+    }
+
+    /**
+     * @param bool $loadCached
+     * @param string $env
+     * @throws Exception\AppKernelException
+     */
+    private function configure(bool $loadCached, string $env): void
+    {
+        if ($this->dev) { // Dev. mode?
+            $loadCached = false; // Always load fresh config
+        }
+
+        // Load cached?
+        if ($loadCached) {
+            try { // Check if cached config exists
+                $cachedConfigFile = $this->directories->cache()
+                    ->file(sprintf('bootstrap.config.%s.php.cache', $env));
+            } catch (DiskException $e) {
+            }
+
+            if (isset($cachedConfigFile)) {
+                try {
+                    try {
+                        $cachedConfig = $cachedConfigFile->read();
+                    } catch (DiskException $e) {
+                        throw new ConfigException('Failed to read cached configuration file');
+                    }
+
+                    $cachedConfig = unserialize($cachedConfig);
+                    if (!$cachedConfig || !$cachedConfig instanceof Config) {
+                        throw new ConfigException('Cached configuration file corrupt or incomplete');
+                    }
+
+                    // Success
+                    $this->config = $cachedConfig;
+                } catch (\Exception $e) {
+                    trigger_error($e->getMessage(), E_USER_WARNING);
+
+                    // Delete cached file...
+                    try {
+                        $cachedConfigFile->delete();
+                    } catch (DiskException $e) {
+                        trigger_error('Failed to delete cached configuration file!', E_USER_WARNING);
+                    }
+                }
+            }
+        }
+
+        // Compile fresh configuration
+        if (!$this->config) {
+            $this->config = new Config($this, $env);
+        }
+
+        // Save in cache?
+        if ($loadCached) {
+            try {
+                $cacheDirectory = $this->directories->cache();
+                $cacheDirectory->write(
+                    sprintf('bootstrap.config.%s.php.cache', $env),
+                    base64_encode(serialize($this->config)),
+                    false,
+                    true
+                );
+            } catch (DiskException $e) {
+                trigger_error(
+                    sprintf('Failed to write bootstrap config. file in cache directory. %s', $e->getMessage()),
+                    E_USER_WARNING
+                );
+            }
+        }
+    }
+
+    /**
+     * @return Config
+     */
+    public function config(): Config
+    {
+        return $this->config;
     }
 
     /**
@@ -103,6 +196,14 @@ class AppKernel
     }
 
     /**
+     * @return bool
+     */
+    public function dev(): bool
+    {
+        return $this->dev;
+    }
+
+    /**
      * @return Directories
      */
     public function directories(): Directories
@@ -110,5 +211,61 @@ class AppKernel
         return $this->directories;
     }
 
+    /**
+     * @return Services
+     */
+    public function services(): Services
+    {
+        return $this->services;
+    }
 
+    /**
+     * @return Cache
+     * @throws \Comely\IO\Cache\Exception\CacheException
+     * @throws \Comely\IO\Cache\Exception\ConnectionException
+     * @throws \Comely\Kernel\Exception\ServicesException
+     */
+    public function cache(): Cache
+    {
+        return $this->services->cache();
+    }
+
+    /**
+     * @return Cipher
+     * @throws \Comely\Kernel\Exception\ServicesException
+     */
+    public function cipher(): Cipher
+    {
+        return $this->services->cipher();
+    }
+
+    /**
+     * @return Translator
+     * @throws Exception\AppKernelException
+     * @throws \Comely\IO\Translator\Exception\LanguageException
+     * @throws \Comely\Kernel\Exception\ServicesException
+     */
+    public function translator(): Translator
+    {
+        return $this->services->translator();
+    }
+
+    /**
+     * @return ComelySession
+     * @throws Exception\AppKernelException
+     * @throws \Comely\Kernel\Exception\ServicesException
+     */
+    public function session(): ComelySession
+    {
+        return $this->services->comelySession();
+    }
+
+    /**
+     * @return Knit
+     * @throws Exception\AppKernelException
+     */
+    public function knit(): Knit
+    {
+        return $this->services->knit();
+    }
 }
---
+++ src/AppKernel/Services.php
@@ -0,0 +1,242 @@
+<?php
+/**
+ * This file is part of Comely Framework Kernel package.
+ * https://github.com/comelyio/framework-kernel
+ *
+ * Copyright (c) 2016-2018 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/comelyio/framework-kernel/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Comely\Framework\AppKernel;
+
+use Comely\Framework\AppKernel;
+use Comely\IO\Cache\Cache;
+use Comely\IO\Cache\Exception\CacheException;
+use Comely\IO\Cipher\Cipher;
+use Comely\IO\Cipher\Keychain\CipherKey;
+use Comely\IO\Session\ComelySession;
+use Comely\IO\Session\Session;
+use Comely\IO\Session\Storage\Disk;
+use Comely\IO\Translator\Translator;
+use Comely\Kernel\Exception\ServicesException;
+use Comely\Knit\Knit;
+
+/**
+ * Class Services
+ * @package Comely\Framework\AppKernel
+ */
+class Services
+{
+    /** @var AppKernel */
+    private $kernel;
+    /** @var null|Cache */
+    private $cache;
+    /** @var null|Cipher */
+    private $cipher;
+    /** @var null|Session */
+    private $session;
+    /** @var null|ComelySession */
+    private $comelySession;
+    /** @var null|Translator */
+    private $translator;
+    /** @var null|Knit */
+    private $knit;
+
+    /**
+     * Services constructor.
+     * @param AppKernel $kernel
+     */
+    public function __construct(AppKernel $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @return Cache
+     * @throws CacheException
+     * @throws ServicesException
+     * @throws \Comely\IO\Cache\Exception\ConnectionException
+     */
+    public function cache(): Cache
+    {
+        if ($this->cache) { // Already registered?
+            return $this->cache;
+        }
+
+        $cacheConfig = $this->kernel->config()->services()->cache();
+        if (!$cacheConfig instanceof AppKernel\Config\App\Services\Cache) {
+            throw ServicesException::ServiceError("cache", 'No configuration found');
+        }
+
+        $engine = $cacheConfig->engine();
+        switch ($engine) {
+            case "redis":
+                $engine = Cache::REDIS;
+                break;
+            case "memcached":
+                $engine = Cache::MEMCACHED;
+                break;
+            default:
+                throw ServicesException::ServiceError("cache", 'Bad engine');
+        }
+
+        $cache = new Cache();
+        $cache->addServer($engine, $cacheConfig->host(), $cacheConfig->port());
+        try {
+            $cache->connect();
+        } catch (CacheException $e) {
+            if ($cacheConfig->terminate() === true) {
+                throw $e;
+            }
+
+            trigger_error($e->getMessage(), E_USER_WARNING);
+        }
+
+        $this->cache = $cache;
+        return $this->cache;
+    }
+
+    /**
+     * @return Translator|null
+     * @throws ServicesException
+     * @throws \Comely\Framework\Exception\AppKernelException
+     * @throws \Comely\IO\Translator\Exception\LanguageException
+     */
+    public function translator(): Translator
+    {
+        if ($this->translator) { // Already registered?
+            return $this->translator;
+        }
+
+        $translatorConfig = $this->kernel->config()->services()->translator();
+        if (!$translatorConfig instanceof AppKernel\Config\App\Services\Translator) {
+            throw ServicesException::ServiceError("translator", 'No configuration found');
+        }
+
+        $translator = Translator::getInstance();
+        $translator->directory($this->kernel->directories()->langs());
+        $translator->fallback($translatorConfig->fallBack());
+
+        // Caching?
+        $caching = $translatorConfig->caching() ?? false;
+        if ($this->kernel->dev()) {
+            $caching = false;
+        }
+
+        if ($caching) {
+            $translator->cacheDirectory($this->kernel->directories()->cache());
+        }
+
+        $this->translator = $translator;
+        return $this->translator;
+    }
+
+    /**
+     * @return Cipher
+     * @throws ServicesException
+     */
+    public function cipher(): Cipher
+    {
+        if ($this->cipher) { // Already registered?
+            return $this->cipher;
+        }
+
+        $cipherConfig = $this->kernel->config()->services()->cipher();
+        if (!$cipherConfig instanceof AppKernel\Config\App\Services\Cipher) {
+            throw ServicesException::ServiceError("cipher", 'No configuration found');
+        }
+
+        $cipher = new Cipher();
+        $count = 0;
+        foreach ($cipherConfig->keys() as $tag => $words) {
+            $key = new CipherKey(hash("sha256", $words));
+            $cipher->keychain()->add($tag, $key); // Append keychain
+
+            if ($count === 0) { // First key?
+                $cipher->defaultKey($key); // Set as default
+            }
+
+            $count++;
+        }
+
+        $this->cipher = $cipher;
+        return $this->cipher;
+    }
+
+    /**
+     * @return Session
+     * @throws ServicesException
+     * @throws \Comely\Framework\Exception\AppKernelException
+     */
+    public function sessions(): Session
+    {
+        if ($this->session) { // Already registered?
+            return $this->session;
+        }
+
+        $sessionsConfig = $this->kernel->config()->services()->sessions();
+        if (!$sessionsConfig instanceof AppKernel\Config\App\Services\Sessions) {
+            throw ServicesException::ServiceError("sessions", 'No configuration found');
+        }
+
+        $sessions = new Session(new Disk($this->kernel->directories()->sessions()));
+        $cookiePath = $sessionsConfig->cookie()->path() ?? null;
+        if (!$cookiePath) {
+            $cookiePath = "/";
+        }
+        $cookieDomain = $sessionsConfig->cookie()->domain() ?? null;
+        if (!$cookieDomain) {
+            $cookieDomain = sprintf('.%s', $this->kernel->config()->project()->domain());
+        }
+
+        $sessions->cookies()->expire($sessionsConfig->cookie()->expire())
+            ->path($cookiePath)
+            ->domain($cookieDomain)
+            ->secure($sessionsConfig->cookie()->secure())
+            ->httpOnly($sessionsConfig->cookie()->httpOnly());
+
+        $this->session = $sessions;
+        $this->comelySession = $sessions->resume(null, $sessionsConfig->cookie()->name());
+
+        return $this->session;
+    }
+
+    /**
+     * @return ComelySession
+     * @throws ServicesException
+     * @throws \Comely\Framework\Exception\AppKernelException
+     */
+    public function comelySession(): ComelySession
+    {
+        if ($this->comelySession) { // Already registered?
+            return $this->comelySession;
+        }
+
+        $this->sessions(); // Load sessions service
+        return $this->comelySession();
+    }
+
+    /**
+     * @return Knit
+     * @throws \Comely\Framework\Exception\AppKernelException
+     */
+    public function knit(): Knit
+    {
+        if ($this->knit) {  // Already registered?
+            return $this->knit;
+        }
+
+        $knit = new Knit();
+        $knit->directories()
+            ->compiler($this->kernel->directories()->compiler())
+            ->caching($this->kernel->directories()->cache());
+
+        $this->knit = $knit;
+        return $this->knit;
+    }
+}
